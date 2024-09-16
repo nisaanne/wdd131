5f2174e89cf8f576517079206bf5bc7463909aad
nisaanne <youremail@nisaanne@byui.edu>
2024-09-15 20:24:51 -0700

    change javascript

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -3,3 +3,8 @@
     document.getElementById('current-year').textContent = currentYear;
 
 };
+
+
+    {let lastModifiedDate = document.lastModified;
+    document.getElementById("lastModified").textContent = "This document was last modified on: " + lastModifiedDate;}
+    
