480356994e368420ae3498204cb9a558c8fdca64
nisaanne <youremail@nisaanne@byui.edu>
2024-09-23 07:03:38 -0700

    add

---
+++ week02/scripts/bom.js
@@ -0,0 +1,42 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+const li = document.createElement('li')
+const deleteButton = document.createElement('button')
+li.textContent = input.value;
+deleteButton.textContent = '//U274C'
+li.append(deleteButton);
+list.append(li)
+
+
+button.addEventListener('click', function() {
+    let inputValue = input.value.trim();
+
+        if (inputValue === '') {
+        alert('Input cannot be blank!');
+        input.focus();
+        return;
+    }
+
+    
+    const li = document.createElement('li');
+    li.textContent = inputValue;
+
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+
+    list.append(li);
+
+    
+    deleteButton.addEventListener('click', function() {
+        list.removeChild(li);
+        input.focus();});
+
+    
+    input.value = '';
+    input.focus();
+});
+
+
