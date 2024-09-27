4b62449287f9cb0ec6a621b677d1eea5893824f0
nisaanne <youremail@nisaanne@byui.edu>
2024-09-26 21:27:10 -0700

    remove extra js and combine two js files

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,3 +4,13 @@ const navMenu = document.getElementById('nav-menu');
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
+
+{var currentYear = new Date().getFullYear();
+
+    document.getElementById('current-year').textContent = currentYear;
+
+};
+
+
+    {let lastModifiedDate = document.lastModified;
+    document.getElementById("lastModified").textContent = "This document was last modified on: " + lastModifiedDate;}
