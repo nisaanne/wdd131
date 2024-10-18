bc4f68d7c810378c69662bb548c78515c407022a
nisaanne <youremail@nisaanne@byui.edu>
2024-10-18 11:48:34 -0700

    last modified message added

--- week05/scripts/forms.js
+++ week05/scripts/forms.js
@@ -1,4 +1,7 @@
-
+{
+  let lastModifiedDate = document.lastModified;
+  document.getElementById("lastModified").textContent = "This document was last modified on: " + lastModifiedDate;
+}
 
 const products = [
     {
