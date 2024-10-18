2bcc66cf528a094bd6d0c6f90c5f2513ae1ea63a
nisaanne <youremail@nisaanne@byui.edu>
2024-10-18 14:29:10 -0700

    fix array

--- sandbox/scripts/debugging.js
+++ sandbox/scripts/debugging.js
@@ -1,59 +1,17 @@
-{
-  let lastModifiedDate = document.lastModified;
-  document.getElementById("lastModified").textContent = "This document was last modified on: " + lastModifiedDate;
-}
+<form method="get" action="review.html" class="wf1">
+<fieldset id="item">
+    <label for="product_name">Product Name<br></br><select id="product_name" name="product_name" required>
+        
+        <option value="" selected disabled>Select a Product ...</option>
+    </select></label>
+</fieldset>
 
-const products = [
-  {
-    id: "fc-1888",
-    name: "flux capacitor",
-    averageting: 4.5,
-  },
-  {
-    id: "fc-2050",
-    name: "power laces",
-    averagerating: 4.7,
-  },
-  {
-    id: "fs-1987",
-    name: "time circuits",
-    averagerating: 3.5,
-  },
-  {
-    id: "ac-2000",
-    name: "low voltage reactor",
-    averagerating: 3.9,
-  },
-  {
-    id: "jj-1969",
-    name: "warp equalizer",
-    averagerating: 5.0,
-  },
-];
-
-const $productName = document.querySelector("#productName");
-
-if ($productName) {
-  products.forEach(product => {
-    let option = document.createElement("option");
-    option.value = product.name;
-    option.textContent = product.name;
-    $productName.appendChild(option);
-  });
-}
-
-function incrementReviewCounter() {
-  let count = localStorage.getItem("reviewCount") || 0;
-  count = parseInt(count) + 1;
-  localStorage.setItem("reviewCount", count);
-  return count;
-}
-document.addEventListener("DOMContentLoaded", function () {
-  if (window.location.pathname.includes("review.html")) {
-    const count = incrementReviewCounter();
-    const counterElement = document.createElement("p");
-    counterElement.textContent = `Total reviews: ${count}`;
-    document.body.appendChild(counterElement);
-  }
-});
-
+<!-- <label for="productName">Product Name:</label>
+                <select id="productName" name="productName" required>
+                    <option value="" disabled selected>Select a product &#9662;</option>
+                    <option value="fc-1888">flux capacitor</option>
+                    <option value="fc-2050">power laces</option>
+                    <option value="fs-1987">time circuits</option>
+                    <option value="ac-2000">low voltage reactor</option>
+                    <option value="jj-1969">warp equalizer</option> 
+                </select> --></form>
