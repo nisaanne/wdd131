997d4dbe50a4212dd26f68d8e4c8c67101f49f4c
nisaanne <youremail@nisaanne@byui.edu>
2024-10-23 07:19:13 -0700

    update form

--- fromtheflames/scripts/flames.js
+++ fromtheflames/scripts/flames.js
@@ -1,3 +1,28 @@
-// {let lastModifiedDate = document.lastModified;
-//     document.getElementById("lastModified").textContent = "This document was last modified on: " + lastModifiedDate;}
+{var currentYear = new Date().getFullYear();
+
+    document.getElementById('current-year').textContent = currentYear;
+
+};
+
     
+document.getElementById('reviewForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    // Get form values
+    const date = document.getElementById('pickdate').value;
+    const name = document.getElementById('username').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
+    
+    // Create an array to store data
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+    
+    // Add new form data to array
+    formData.push({ date, name, phone, email });
+    
+    // Save array to local storage
+    localStorage.setItem('formData', JSON.stringify(formData));
+    
+    alert('Form data saved!');
+    document.getElementById('reviewForm').reset();
+});
