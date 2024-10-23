2d81f2cd90f6bcef981d0df410e7f6c9815cac56
nisaanne <youremail@nisaanne@byui.edu>
2024-10-23 12:51:29 -0700

    form validation

--- fromtheflames/scripts/flames.js
+++ fromtheflames/scripts/flames.js
@@ -8,19 +8,36 @@
 document.getElementById('reviewForm').addEventListener('submit', function(e) {
     e.preventDefault();
     
-    // Get form values
-    const date = document.getElementById('pickdate').value;
+       const date = document.getElementById('pickdate').value;
     const name = document.getElementById('username').value;
     const phone = document.getElementById('phone').value;
     const email = document.getElementById('email').value;
     
-    // Create an array to store data
+
+    if (!date) {
+        alert('Please choose a date.');
+        return;
+    }
+    if (!name) {
+        alert('Please enter your name.');
+        return;
+    }
+    if (!phone) {
+        alert('Please enter your phone number.');
+        return;
+    }
+    if (!email) {
+        alert('Please enter your email address.');
+        return;
+    }
+    
+   
     let formData = JSON.parse(localStorage.getItem('formData')) || [];
     
-    // Add new form data to array
+  
     formData.push({ date, name, phone, email });
     
-    // Save array to local storage
+  
     localStorage.setItem('formData', JSON.stringify(formData));
     
     alert('Form data saved!');
